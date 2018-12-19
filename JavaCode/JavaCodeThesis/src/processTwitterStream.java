ef4d944d3a89b08d426e5e614481544ff0a4446c
Eli Brunette <elibrunette@eagles.ewu.edu>
2018-12-19 15:26:17 -0800

    Moving over the eclipse package
    Moved over the eclipse package and created files from the program.

--- JavaCode/JavaCodeThesis/src/processTwitterStream.java
+++ JavaCode/JavaCodeThesis/src/processTwitterStream.java
@@ -20,10 +20,10 @@ public class processTwitterStream {
 	*/
 	
 	//TODO Change me!
-	private static String _filePath = "F:/twitterText.txt";
-	private static String _outputFile = "F:/Thesis/processedTwitterData1.csv";
-	private static String _badOutputFile = "F:/Thesis/badProccedTwitterData1.csv";
-	private static String _statistics = "F:/Thesis/statistic1.txt";
+	private static String _filePath = "C:/Users/ebrunette/Documents/Presentation/PyDataTwitterPresentation/PythonCode/twitterText.txt";
+	private static String _outputFile = "C:/Users/ebrunette/Documents/Presentation/PyDataTwitterPresentation/JavaCode/JavaCodeThesis/processedTwitterData.csv";
+	private static String _badOutputFile = "C:/Users/ebrunette/Documents/Presentation/PyDataTwitterPresentation/JavaCode/JavaCodeThesis/badProccedTwitterData.csv";
+	private static String _statistics = "C:/Users/ebrunette/Documents/Presentation/PyDataTwitterPresentation/JavaCode/JavaCodeThesis/statistic.txt";
 	
 	
 	private static PrintWriter _processedTweet;
@@ -32,14 +32,12 @@ public class processTwitterStream {
 
 	private static JsonObject _tweet;
 	private static JsonObject _user;
-	private static JsonObject _place;
 	private static JsonObject _entities;
 	private static JsonArray _hashtags;
 	private static JsonObject _delete;
 
 	private static double _total;
-	private static double _geoTotal;
-	private static double _geoUseful;
+	private static double _retweetedTotal;
 
 	public static void main(String... args) throws FileNotFoundException {
 
@@ -59,10 +57,9 @@ public class processTwitterStream {
 				establishClassLevelVariables();
 				if (_delete == null) {
 					JsonElement[] desiredElements = getDesiredElements();
-					if (isGeoEnabled() || desiredElements[5] != null) {
+					if (!retweeted(desiredElements)) {
 						if (isInEnglish(desiredElements)) {
 							outputTheDesiredElements(desiredElements);
-							_geoTotal++;
 						}
 					}
 				}
@@ -74,12 +71,10 @@ public class processTwitterStream {
 			} finally {
 				if (_total > 100000) {
 					System.out.println("Processed 100000 records");
-					_processedStatistics.println("Total Count::" + _total + "::Total Geo_Enabled::" + _geoTotal
-							+ "::Percentage Geo_Enabled::" + (_geoTotal / _total) + "::Total Useful::" + _geoUseful
-							+ "::Percentage Useful::" + (_geoUseful / _total));
+					_processedStatistics.println("Total Tweets Processed:" + _total + "::Total amount of retweets:" +
+													_retweetedTotal + "::Percentage of retweets:" +(_retweetedTotal/_total));//Output processed Statistics
 					_total = 0;
-					_geoTotal = 0;
-					_geoUseful = 0;
+					_retweetedTotal = 0;
 				}
 				if (_delete != null)
 					_delete = null;
@@ -89,9 +84,8 @@ public class processTwitterStream {
 			}
 		}
 
-		_processedStatistics.println("Total Count::" + _total + "::Total Geo_Enabled::" + _geoTotal
-				+ "::Percentage Geo_Enabled::" + (_geoTotal / _total) + "::Total Useful::" + _geoUseful
-				+ "::Percentage Useful::" + (_geoUseful / _total));
+		_processedStatistics.println("Total Tweets Processed:" + _total + "::Total amount of retweets:" 
+										+ _retweetedTotal + "::Percentage of retweets:" +(_retweetedTotal/_total));//Output what we want to see in the statistics
 		fin.close();
 		_badProcessedTweet.close();
 		_processedTweet.close();
@@ -100,22 +94,18 @@ public class processTwitterStream {
 
 	}
 
-	private static boolean isInEnglish(JsonElement[] desiredElements) {
-		if (desiredElements[7].getAsString().equals("en"))// check this is right index
+	private static boolean retweeted(JsonElement[] theElements) {
+		String tweetText = theElements[2].getAsString();
+		if(tweetText.startsWith("RT @")) {
+			_retweetedTotal++;
 			return true;
+		}
 		return false;
 	}
 
-	private static boolean isGeoEnabled() {
-		if (_user != null) {
-			Object geo = _user.get("geo_enabled");
-			JsonElement isGeoEnabled = null;
-			if (!(geo instanceof JsonNull)) {
-				isGeoEnabled = (JsonElement) geo;
-				if (isGeoEnabled.getAsString().equals("true"))
-					return true;
-			}
-		}
+	private static boolean isInEnglish(JsonElement[] desiredElements) {
+		if (desiredElements[4].getAsString().equals("en"))// check this is right index
+			return true;
 		return false;
 	}
 
@@ -124,9 +114,6 @@ public class processTwitterStream {
 	}
 
 	private static void outputTheDesiredElements(JsonElement[] desiredElements) {
-
-		if (desiredElements[5] == null)
-			return;
 
 		String toOutput = desiredElements[0].getAsString();
 		for (int x = 1; x < desiredElements.length; x++) {
@@ -144,21 +131,16 @@ public class processTwitterStream {
 			toOutput = toOutput + "::" + hash;
 		}
 		_processedTweet.println(toOutput);
-		_processedTweet.println("");
-		_geoUseful++;
 	}
 
 	private static JsonElement[] getDesiredElements() {
-		JsonElement[] theElements = new JsonElement[8];
+		JsonElement[] theElements = new JsonElement[5];
 
-		theElements[0] = getCreated();
-		theElements[1] = getTweetID();
-		theElements[2] = getUserID();
-		theElements[3] = _tweet.get("text");
-		theElements[4] = getLocation();
-		theElements[5] = getCoordinates();
-		theElements[6] = getNameOfPlace();
-		theElements[7] = getLang();
+		theElements[0] = getTweetID();
+		theElements[1] = getCreated();
+		theElements[2] = _tweet.get("text");
+		theElements[3] = getUserID();
+		theElements[4] = getLang();
 
 		return theElements;
 	}
@@ -199,17 +181,14 @@ public class processTwitterStream {
 	private static void establishClassLevelVariables() {
 		Object tweet = _tweet;
 		Object user = _tweet.get("user");
-		Object place = _tweet.get("place");
 		Object entities = _tweet.get("entities");
 		Object delete = _tweet.get("delete");
-		if (delete instanceof JsonObject) {
+		if (delete instanceof JsonObject) { //check for edge case of a deletedf tweet
 			_delete = _tweet.getAsJsonObject("delete");
 			return;
 		}
 		if (!(user instanceof JsonNull))
 			_user = _tweet.getAsJsonObject("user");
-		if (!(place instanceof JsonNull))
-			_place = _tweet.getAsJsonObject("place");
 		try {
 			_entities = (JsonObject) entities;
 			_hashtags = _entities.getAsJsonArray("hashtags");
@@ -219,138 +198,9 @@ public class processTwitterStream {
 
 	}
 
-	private static JsonElement getLocation() {
-		if (_tweet != null)
-			if (_user != null)
-				return _user.get("location");
-
-		return null;
-	}
-
-	private static JsonElement getCoordinates() {
-		JsonElement firstLocation = findCoordinatesFromCoordinates();
-		if (firstLocation != null)
-			return firstLocation;
-		JsonElement fourthLocation = findCoordinatesFromGeo();
-		if (fourthLocation != null)
-			return fourthLocation;
-		JsonElement thirdLocation = findCoordinatesFromUserLocation();
-		if (thirdLocation != null)
-			return thirdLocation;
-		JsonElement secondLocation = processBoundingBox();
-		if (secondLocation != null)
-			return secondLocation;
-
-		return null;
-	}
-
-	private static JsonElement findCoordinatesFromGeo() {
-		Object geo = _tweet.get("geo");
-		if (!(geo instanceof JsonNull)) {
-			JsonObject geoObject = (JsonObject) geo;
-			JsonElement coordinates = geoObject.get("coordinates");
-			return coordinates;
-		}
-		return null;
-	}
-
-	private static JsonElement findCoordinatesFromCoordinates() {
-		try {
-			JsonObject coordinates = (JsonObject) _tweet.get("coordinates");
-			if (coordinates != null)
-				return coordinates.get("coordinates");
-			return null;
-		} catch (ClassCastException e) {
-			return null;
-		}
-
-	}
-
-	private static JsonElement processBoundingBox() {
-		JsonArray boundingBox = getBoundingBox();
-		if (boundingBox != null) {
-			return generateCentroid(boundingBox);
-		}
-		return null;
-	}
-
-	private static JsonElement generateCentroid(JsonArray boundingBox) {
-		Gson bound = new Gson();
-		float[][][] box = bound.fromJson(boundingBox, float[][][].class);
-		return computeControid(box).get("coordinates");
-	}
-
-	private static JsonObject computeControid(float[][][] box) {
-		float runningLat = 0;
-		float runningLong = 0;
-		for (int x = 0; x < box[0].length; x++) {
-			for (int y = 0; y < box[0][x].length; y++) {
-				if (y == 0)
-					runningLat = runningLat + box[0][x][y];
-				else if (y == 1)
-					runningLong = runningLong + box[0][x][y];
-			}
-		}
-		String toReturn = "{\"coordinates\":[" + (runningLat / box[0].length) + "," + (runningLong / box[0].length)
-				+ "]}";
-		JsonParser jsonParser = new JsonParser();
-		JsonObject jo = (JsonObject) jsonParser.parse(toReturn);
-		return jo;
-
-	}
-
-	private static JsonArray getBoundingBox() {
-		try {
-			if (_place != null) {
-				JsonObject bounding_box = (JsonObject) _place.get("bounding_box");
-				if (bounding_box != null) {
-					JsonElement bBox = bounding_box.get("coordinates");
-					if (bBox instanceof JsonArray)
-						return (JsonArray) bBox;
-				}
-			}
-
-			return null;
-		} catch (ClassCastException e) {
-			return null;
-		}
-
-	}
-
-	private static JsonElement findCoordinatesFromUserLocation() {
-		try {
-			if (_user != null) {
-				JsonObject derived = (JsonObject) _user.get("derived");
-				if (derived != null) {
-					JsonObject location = (JsonObject) derived.get("location");
-					if (location != null) {
-						JsonObject geo = (JsonObject) location.get("geo");
-						if (geo != null)
-							return geo.get("coordinates");
-					}
-				}
-			}
-			return null;
-
-		} catch (ClassCastException e) {
-			return null;
-		}
-	}
-
 	private static Scanner getScanner() throws FileNotFoundException {
 		Scanner toReturn = new Scanner(new File(_filePath));
 		return toReturn;
-	}
-
-	public static JsonElement getNameOfPlace() {
-		try {
-			if (_place != null) {
-				return _place.get("full_name");
-			}
-		} catch (ClassCastException e) {
-			return null;
-		}
-		return null;
 	}
 
 	private static JsonElement getLang() {
